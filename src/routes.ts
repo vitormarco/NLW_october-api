67c0cf1c9b9b9601daa2dafad4b8ea1b0f0d2922
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-16 19:59:10 -0300

    Listar orfanatos

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import OrphanagesController from './Controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
