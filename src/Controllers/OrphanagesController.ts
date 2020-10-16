816c812c2a63f25420194711aff80146435498b2
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-16 20:47:49 -0300

    upload de imagens

--- src/Controllers/OrphanagesController.ts
+++ src/Controllers/OrphanagesController.ts
@@ -35,7 +35,12 @@ export default {
     } = request.body;
   
     const orphanagesRepository = getRepository(Orphanage);
-  
+
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -44,6 +49,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images
     });
   
     await orphanagesRepository.save(orphanage);
